2a0c33124f0852888fa1d9883360a8ae7fd9e937
bcwan <bcwan8@gmail.com>
2020-05-10 20:32:04 -0700

    Add heroku dependencies

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const db = require("./config/keys").mongoURI;
 const mongoose = require("mongoose");
 // parse JSON we send to front end
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
@@ -29,3 +30,10 @@ app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
